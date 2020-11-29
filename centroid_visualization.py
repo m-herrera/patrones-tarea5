6840d7e059986e1356b5be463a4ad4c7e1fd9f19
Jasson Rodriguez <jassonrm@icloud.com>
2020-11-28 19:13:58 -0600

    Add new graphs to 28x28

--- centroid_visualization.py
+++ centroid_visualization.py
@@ -2,7 +2,15 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 
 
-def plot_centroid(data, k, shape=28, init='k-means++', n_init=10):
+def plot_centroid(data, k, dataset='mnist', init='k-means++', n_init=10):
+
+    if(dataset == 'mnist'):
+        shape = 28
+        max_value = 255
+    else:
+        shape = 8
+        max_value = 16
+
     full_dimensions = KMeans(init=init, n_clusters=k, n_init=n_init)
     full_dimensions.fit(data)
     centroids = full_dimensions.cluster_centers_
@@ -15,5 +23,7 @@ def plot_centroid(data, k, shape=28, init='k-means++', n_init=10):
 
     for i in range(k):
         plt.subplot(2, cols, i + 1)
-        plt.imshow(images[i], cmap='gray', vmin=0, vmax=255)
+        plt.imshow(images[i], cmap='gray', vmin=0, vmax=max_value)
     plt.show()
+
+    return centroids
--- plot_kmeans_digits_8.py
+++ plot_kmeans_digits_8.py
@@ -38,12 +38,12 @@ from sklearn.cluster import KMeans
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import scale
-from tensorflow.keras.datasets import mnist
 
 np.random.seed(42)
 
-(X_digits, y_digits), (x_test, y_test) = mnist.load_data()
-data = X_digits.reshape(60000, 784)
+X_digits, y_digits = load_digits(return_X_y=True)
+data = X_digits
+print(data[0])
 
 n_samples, n_features = data.shape
 n_digits = len(np.unique(y_digits))
@@ -53,6 +53,7 @@ sample_size = 300
 
 print("n_digits: %d, \t n_samples %d, \t n_features %d"
       % (n_digits, n_samples, n_features))
+
 
 print(82 * '_')
 print('init\t\ttime\tinertia\thomo\tcompl\tv-meas\tARI\tAMI\tsilhouette')
@@ -67,27 +68,28 @@ def bench_k_means(estimator, name, data):
              metrics.completeness_score(labels, estimator.labels_),
              metrics.v_measure_score(labels, estimator.labels_),
              metrics.adjusted_rand_score(labels, estimator.labels_),
-             metrics.adjusted_mutual_info_score(labels, estimator.labels_),
+             metrics.adjusted_mutual_info_score(labels,  estimator.labels_),
              metrics.silhouette_score(data, estimator.labels_,
                                       metric='euclidean',
                                       sample_size=sample_size)))
 
+bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
+              name="k-means++", data=data)
 
-# bench_k_means(KMeans(init='k-means++', n_clusters=n_digits, n_init=10),
-#               name="k-means++", data=data)
-#
-# bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),
-#               name="random", data=data)
-#
-# # in this case the seeding of the centers is deterministic, hence we run the
-# # kmeans algorithm only once with n_init=1
-# pca = PCA(n_components=n_digits).fit(data)
-# bench_k_means(KMeans(init=pca.components_, n_clusters=n_digits, n_init=1),
-#               name="PCA-based",
-#               data=data)
+bench_k_means(KMeans(init='random', n_clusters=n_digits, n_init=10),
+              name="random", data=data)
+
+# in this case the seeding of the centers is deterministic, hence we run the
+# kmeans algorithm only once with n_init=1
+pca = PCA(n_components=n_digits).fit(data)
+bench_k_means(KMeans(init=pca.components_, n_clusters=n_digits, n_init=1),
+              name="PCA-based",
+              data=data)
 print(82 * '_')
 
-plot_centroid(data, 3)
+plot_centroid(data, 3, dataset='digits')
+plot_centroid(data, 10, dataset='digits')
+plot_centroid(data, 20, dataset='digits')
 
 # #############################################################################
 # Visualize the results on PCA-reduced data
@@ -96,15 +98,12 @@ reduced_data = PCA(n_components=2).fit_transform(data)
 kmeans = KMeans(init='k-means++', n_clusters=n_digits, n_init=10)
 kmeans.fit(reduced_data)
 
-print("Reduce data ", reduced_data.shape)
+# Step size of the mesh. Decrease to increase the quality of the VQ.
+h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
 
 # Plot the decision boundary. For that, we will assign a color to each
 x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
 y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-
-# Step size of the mesh. Decrease to increase the quality of the VQ.
-h = (x_max - x_min) / 2000  # point in the mesh [x_min, x_max]x[y_min, y_max].
-
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
 # Obtain labels for each point in mesh. Use last trained model.
